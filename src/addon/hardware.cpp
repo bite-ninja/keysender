15cdddcebfad2c0a6aec7e37e56abde031cd854a
Sam Spycher <samspycher@users.noreply.github.com>
2024-01-31 15:36:49 +0100

    fix: wire JS interface to C

--- src/addon/hardware.cpp
+++ src/addon/hardware.cpp
@@ -196,6 +196,7 @@ Napi::Object Hardware::Init(Napi::Env env, Napi::Object exports) {
           InstanceMethod("toggleMb", &Hardware::toggleMb),
           InstanceMethod("move", &Hardware::move),
           InstanceMethod("scrollWheel", &Hardware::scrollWheel),
+          InstanceMethod("showCursor", &Hardware::showCursor),
           InstanceMethod("toggleKey", &Hardware::toggleKey),
           InstanceMethod("printChar", &Hardware::printChar),
           InstanceMethod("isOpen", &Hardware::isOpen),
